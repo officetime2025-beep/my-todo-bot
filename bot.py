8f55e63bca9fde4f07f6d676aaba2ff72f6d3ee6
officetime2025-beep <officetime2025@gmail.com>
2025-10-23 16:52:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 # Токен из переменной среды (задаётся в Railway)
-TOKEN = os.environ["8402424321:AAH-LHIMD1X_0ehxO5joFNLp8fePNYND76g"]
+TOKEN = os.environ["TOKEN"]
 
 # Категории
 CATEGORIES = [
@@ -218,4 +218,5 @@ if __name__ == "__main__":
 
     print("✅ Бот запущен!")
     app.run_polling()
+
 
